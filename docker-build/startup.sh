67fa536029767d4a0be737ccb7706bdd16d9044c
Davide Motte <dmotte@dmotte.fakemail>
2021-04-06 22:47:14 +0200

    Add VNC_PASSWORD option

--- docker-build/startup.sh
+++ docker-build/startup.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
-set -ex
+set -e
 
-# TODO do preliminary stuff
+if [ -n "$VNC_PASSWORD" ]; then
+    sed -i "s/###VNC_PASSWORD###/$VNC_PASSWORD/" /etc/supervisor/supervisord.conf
+    unset VNC_PASSWORD
+    echo "VNC password set in supervisord.conf"
+else
+    sed -i "s/-passwd \"###VNC_PASSWORD###\"//" /etc/supervisor/supervisord.conf
+    echo "VNC password option removed from supervisord.conf"
+fi
 
 # Start all the services
 /usr/bin/supervisord
