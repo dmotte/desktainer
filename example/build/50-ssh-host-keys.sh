40bdce2c8bbc21902d76ac748221dc688044ec63
dmotte <37443982+dmotte@users.noreply.github.com>
2023-07-09 22:30:04 +0200

    Improved default SSH host keys removal in example

--- example/build/50-ssh-host-keys.sh
+++ example/build/50-ssh-host-keys.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Get host keys from /etc/ssh/host-keys
+# Get host keys from the volume
 rm -f /etc/ssh/ssh_host_*_key /etc/ssh/ssh_host_*_key.pub
 cp /etc/ssh/host-keys/ssh_host_*_key /etc/ssh/ 2>/dev/null || true
 cp /etc/ssh/host-keys/ssh_host_*_key.pub /etc/ssh/ 2>/dev/null || true
@@ -13,6 +13,6 @@ chown root:root /etc/ssh/ssh_host_*_key /etc/ssh/ssh_host_*_key.pub
 chmod 600 /etc/ssh/ssh_host_*_key
 chmod 644 /etc/ssh/ssh_host_*_key.pub
 
-# Copy the (previously missing) generated host keys to /etc/ssh/host-keys
+# Copy the (previously missing) generated host keys to the volume
 cp -n /etc/ssh/ssh_host_*_key /etc/ssh/host-keys/ 2>/dev/null || true
 cp -n /etc/ssh/ssh_host_*_key.pub /etc/ssh/host-keys/ 2>/dev/null || true
