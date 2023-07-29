7203420ecfd7455fd2378418feaded76dcc70dfa
dmotte <37443982+dmotte@users.noreply.github.com>
2023-07-29 19:48:14 +0200

    Replaced Bash true with colon

--- example/build/setup/main.sh
+++ example/build/setup/main.sh
@@ -80,8 +80,8 @@ mkdir /etc/ssh/host-keys
 cat << 'EOF' > /opt/startup-late/50-ssh-host-keys.sh
 # Get host keys from the volume
 rm -f /etc/ssh/ssh_host_*_key /etc/ssh/ssh_host_*_key.pub
-cp /etc/ssh/host-keys/ssh_host_*_key /etc/ssh/ 2>/dev/null || true
-cp /etc/ssh/host-keys/ssh_host_*_key.pub /etc/ssh/ 2>/dev/null || true
+cp /etc/ssh/host-keys/ssh_host_*_key /etc/ssh/ 2>/dev/null || :
+cp /etc/ssh/host-keys/ssh_host_*_key.pub /etc/ssh/ 2>/dev/null || :
 
 # Generate the missing host keys
 ssh-keygen -A
@@ -92,8 +92,8 @@ chmod 600 /etc/ssh/ssh_host_*_key
 chmod 644 /etc/ssh/ssh_host_*_key.pub
 
 # Copy the (previously missing) generated host keys to the volume
-cp -n /etc/ssh/ssh_host_*_key /etc/ssh/host-keys/ 2>/dev/null || true
-cp -n /etc/ssh/ssh_host_*_key.pub /etc/ssh/host-keys/ 2>/dev/null || true
+cp -n /etc/ssh/ssh_host_*_key /etc/ssh/host-keys/ 2>/dev/null || :
+cp -n /etc/ssh/ssh_host_*_key.pub /etc/ssh/host-keys/ 2>/dev/null || :
 EOF
 
 ################################ Shell In A Box ################################
