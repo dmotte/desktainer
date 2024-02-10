e7af755111a61d57b09ac6c5ffa9752caf846c1a
dmotte <37443982+dmotte@users.noreply.github.com>
2024-02-10 12:59:55 +0100

    Shortened portmap SSH command

--- example/build/setup/main.sh
+++ example/build/setup/main.sh
@@ -118,9 +118,9 @@ echo 'install -d -omainuser -gmainuser -m700 /data/mainuser' \
 
 # cat << 'EOF' > /etc/supervisor/conf.d/portmap-ssh.conf
 # [program:portmap-ssh]
-# command=/bin/bash -ec 'ssh -i /home/mainuser/.ssh/portmap-ssh.pem \
-#     -o ServerAliveInterval=30 -o ExitOnForwardFailure=yes \
-#     myuser@myserver.example.com -Nv -R 12345:127.0.0.1:22 \
+# command=/bin/bash -ec 'ssh -i ~/.ssh/portmap-ssh.pem \
+#     -oServerAliveInterval=30 -oExitOnForwardFailure=yes \
+#     myuser@myserver.example.com -NvR12345:127.0.0.1:22 \
 #     || result=$?; sleep 30; exit "${result:-0}"'
 # startsecs=0
 # priority=10
